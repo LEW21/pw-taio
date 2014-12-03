a16c7013485818c152d6a56921d6c714e4c375c0
Jakub Gocławski <gocek13@gmail.com>
2014-12-03 14:40:26 +0100

    Generowanie zbiorow z elementami obcymi.

--- src/lib/set_generator.py
+++ src/lib/set_generator.py
@@ -1,5 +1,6 @@
 import random
 
+
 class DataSetGenerator:
     test_set_size_factor = 1/3
     attribute_range = (0, 100)
@@ -36,4 +37,39 @@ class DataSetGenerator:
                 element_attributes = [round(random.normalvariate(attribute, self.sigma_absolute))
                                       for attribute in representative]
                 data_set.append([class_number, element_attributes])
+        return data_set
+
+
+class ForeignObjectsSetGenerator(DataSetGenerator):
+    @property
+    def foreign_class_number(self):
+        """Foreign class number is the last class number + 1."""
+        return self.classes_count
+
+
+class UnifiedForeignObjectsSetGenerator(ForeignObjectsSetGenerator):
+    def generate_set(self, rows_per_class):
+        data_set = super().generate_set(rows_per_class)
+        foreign_class_number = self.foreign_class_number
+        min_attribute = self.attribute_range[0]
+        max_attribute = self.attribute_range[1]
+        for i in range(0, rows_per_class):
+            element_attributes = [round(random.randrange(min_attribute, max_attribute))
+                                  for _ in range(0, self.attributes_count)]
+            data_set.append([foreign_class_number, element_attributes])
+        return data_set
+
+
+class PermutedForeignObjectsSetGenerator(ForeignObjectsSetGenerator):
+    def generate_set(self, rows_per_class):
+        data_set = super().generate_set(rows_per_class)
+        foreign_class_number = self.foreign_class_number
+        for i in range(0, rows_per_class):
+            element_attributes = []
+            for j in range(0, self.attributes_count):
+                random_class_number = random.randrange(self.classes_count)
+                representative = self.class_representative[random_class_number]
+                random_attribute_number = random.randrange(self.attributes_count)
+                element_attributes.append(representative[random_attribute_number])
+            data_set.append([foreign_class_number, element_attributes])
         return data_set
